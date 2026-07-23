77cb49d11d7bd72dec765d2e29f15db0a2cfc06d
Christian Abalos <abaloschristianjamess@gmail.com>
2026-07-23 09:19:57 +0800

    Fix AppServiceProvider boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-         Paginator::useBootstrapFive();
+        URL::forceScheme('https');
+
+        Paginator::useBootstrapFive();
     }
 }
